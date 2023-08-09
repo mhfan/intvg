d0b621d36a0ac0ee784a60ad769608b4461936a1
MeiHui FAN <mhfan@ustc.edu>
2023-08-09 17:49:51 +0800

    fix to run build.rs on any update

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 
 fn main() {     // https://doc.rust-lang.org/stable/cargo/reference/build-scripts.html
     // By default, cargo always re-run the build script if any file within the package
-    // is changed, if no any rerun-if instruction is emitted.
+    // is changed, and no any rerun-if instruction is emitted.
+    println!("cargo:rerun-if-changed=src/");
 
-    //println!("cargo:rerun-if-changed=.git/index");
+    println!("cargo:rerun-if-changed=.git/index");
     let output = std::process::Command::new("git")
         .args(["rev-parse", "--short", "HEAD"]).output().unwrap();
     println!("cargo:rustc-env=BUILD_GIT_HASH={}", String::from_utf8(output.stdout).unwrap());
